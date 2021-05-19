9a577cca5aea1951139123a964e4fb2ea3d2ee1a
Beth Gooding <beth.gooding@hotmail.com>
2021-05-19 15:37:35 +0100

    Adds persisting data exercise code

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from 'react';
 import { MoodOption, MoodOptionWithTimeStamp } from './types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type AppContextType = {
   moodList: MoodOptionWithTimeStamp[];
@@ -13,12 +14,52 @@ const defaultValue = {
 
 const AppContext = createContext<AppContextType>(defaultValue);
 
+const storageKey = 'mood-tracker-data';
+
+type AppData = {
+  moods: MoodOptionWithTimeStamp[];
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const data = await AsyncStorage.getItem(storageKey);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch {
+    return null;
+  }
+};
+
+const setAppData = async (newData: AppData) => {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(newData));
+  } catch {}
+};
+
 export const AppProvider: React.FC = ({ children }) => {
   const [moodList, setMoodList] = React.useState<MoodOptionWithTimeStamp[]>([]);
   const handleAddMood = React.useCallback((newMood: MoodOption) => {
     setMoodList(current => {
-      return [...current, { moodOption: newMood, timestamp: Date.now() }];
+      const updatedMoods = [
+        ...current,
+        { moodOption: newMood, timestamp: Date.now() },
+      ];
+      setAppData({ moods: updatedMoods });
+      return updatedMoods;
     });
+  }, []);
+  React.useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getAppData();
+
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
   }, []);
   return (
     <AppContext.Provider value={{ moodList, handleAddMood }}>
