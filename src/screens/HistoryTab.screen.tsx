b55fc2c91870d360344a72c7882b574c147f8905
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-20 09:07:38 +0100

    Makes history tab a scrollview

--- src/screens/HistoryTab.screen.tsx
+++ src/screens/HistoryTab.screen.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import { useAppContext } from '~src/App.provider';
 import { MoodItemRow } from '~src/components/MoodItemRow';
 
 export const HistoryTab: React.FC = () => {
   const { moodList } = useAppContext();
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {moodList.map(mood => (
         <MoodItemRow mood={mood} key={mood.timestamp} />
       ))}
-    </View>
+    </ScrollView>
   );
 };
 
