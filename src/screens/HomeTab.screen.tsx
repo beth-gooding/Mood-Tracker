8db4a44a6108a01d5053c573c3405f278d6171f7
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-20 09:31:05 +0100

    Adds Image code

--- src/screens/HomeTab.screen.tsx
+++ src/screens/HomeTab.screen.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Image, ImageBackground } from 'react-native';
 import { MoodPicker } from '~src/components/MoodPicker';
 import { useAppContext } from '~src/App.provider';
+
+const imageSrc = require('~src/assets/images/ying-yang.png');
+const networkImageUrl =
+  'https://images.unsplash.com/photo-1494459940152-1e911caa8cc5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80';
 
 export const HomeTab = () => {
   const { handleAddMood } = useAppContext();
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      source={{ uri: networkImageUrl }}
+      style={styles.container}
+      accessibilityLabel="Sunset at a beach">
+      <Image
+        source={imageSrc}
+        style={styles.image}
+        accessibilityLabel="ying-yang icon"
+      />
+
       <MoodPicker onAddMood={handleAddMood} />
-    </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  image: {
+    alignSelf: 'center',
+    marginBottom: 10,
   },
 });
