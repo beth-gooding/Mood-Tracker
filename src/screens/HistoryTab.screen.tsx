5f062a4e23c5f3a19e4ce750c88def79bc8f9b9b
Beth Gooding <beth.gooding@brightnetwork.co.uk>
2021-05-20 12:07:49 +0100

    Adds drawer and animation in history tab

--- src/screens/HistoryTab.screen.tsx
+++ src/screens/HistoryTab.screen.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
 import { useAppContext } from '~src/App.provider';
 import { MoodItemRow } from '~src/components/MoodItemRow';
+import orderBy from 'lodash/orderBy';
+import groupBy from 'lodash/groupBy';
+import { format } from 'date-fns';
+import { MoodOptionWithTimeStamp } from '~src/types';
+import { Drawer } from '~src/components/Drawer';
 
 export const HistoryTab: React.FC = () => {
   const { moodList } = useAppContext();
+  const days = React.useMemo(() => {
+    const ordered = orderBy(moodList, 'timestamp', 'desc');
+
+    const grouped = groupBy(ordered, item =>
+      format(new Date(item.timestamp), 'dd MMM, yyyy'),
+    );
+
+    return Object.entries(grouped).map(([day, moodsInDay]) => ({
+      day,
+      moodsInDay,
+    }));
+  }, [moodList]);
   return (
-    <ScrollView style={styles.container}>
-      {moodList.map(mood => (
-        <MoodItemRow mood={mood} key={mood.timestamp} />
-      ))}
-    </ScrollView>
+    <FlatList
+      keyExtractor={item => item.day}
+      data={days}
+      renderItem={({ item }) => (
+        <Drawer title={item.day}>
+          {item.moodsInDay.map((mood: MoodOptionWithTimeStamp) => (
+            <MoodItemRow mood={mood} key={mood.timestamp} />
+          ))}
+        </Drawer>
+      )}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
